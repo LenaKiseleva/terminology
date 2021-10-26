e7d63c104a53e198071718285d0a36b76db50357
LenaKiseleva <ellen91981@mail.ru>
2021-10-27 00:10:45 +0300

    Replace field to __all__

--- api/serializers.py
+++ api/serializers.py
@@ -7,20 +7,11 @@ class UnitManualSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UnitManual
-        fields = ('id', 'code_unit', 'value_unit', 'pub_date', )
+        fields = '__all__'
 
 
 class ManualSerializer(serializers.ModelSerializer):
-    manual = UnitManualSerializer(source='units', many=True)
 
     class Meta:
         model = Manual
-        fields = ('id', 'name', 'short_name', 'description',
-                  'version', 'commencement_date', 'manual', )
-
-
-class UnitManualListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UnitManual
-        fields = ('id', 'code_unit', 'value_unit', 'pub_date', 'manual')
+        fields = '__all__'
